e938d104d3fa17b2d87ef207c0ed9fc8bbce9be2
unknown <4201@4201-03.(none)>
2012-03-28 09:30:24 -0400

    Add solution for 03

--- 03/03.hs
+++ 03/03.hs
@@ -1,4 +1,9 @@
 number = 600851475143
+
+factor :: Integral a => a -> [a]
+factor 1 = 1
 factor x = 
 	let factors p = filter (\a -> p `mod` a == 0) [2.. round (sqrt (fromIntegral p))]
-		smallestFactor = 
+		smallestFactor = if (factors /= []) then (minimum factors) else x 
+		quotient = x / smallestFactor
+	in  [smallestFactor] ++ factor quotient
