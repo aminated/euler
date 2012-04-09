9f6fae3ebe19107c4f590f19e44ca5b0f4148ed2
unknown <4201@4201-03.(none)>
2012-04-09 08:58:06 -0400

    Change 03 solution.

--- 03/03.hs
+++ 03/03.hs
@@ -1,9 +1,10 @@
 number = 600851475143
 
 factor :: Integral a => a -> [a]
-factor 1 = 1
+factor 1 = [1]
 factor x = 
-	let factors p = filter (\a -> p `mod` a == 0) [2.. round (sqrt (fromIntegral p))]
-		smallestFactor = if (factors /= []) then (minimum factors) else x 
-		quotient = x / smallestFactor
-	in  [smallestFactor] ++ factor quotient
+    let factors = filter (\a -> x `mod` a == 0) [2.. round (sqrt (fromIntegral x))]
+        smallestFactor = if (factors /= []) then (minimum factors) else x 
+        quotient = x `div` smallestFactor
+    in  [smallestFactor] ++ factor quotient
+solution = factor number
